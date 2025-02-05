e2727ec7db7b1e056142dc6f130e320497d4c4aa
Diego Flores <124117944+diegnghtmr@users.noreply.github.com>
2025-02-04 23:43:37 -0500

    First

---
+++ src/main/java/co/edu/uniquindio/programacion4/seguimiento/Ejercicio1.java
@@ -0,0 +1,9 @@
+package co.edu.uniquindio.programacion4.seguimiento;
+
+/*
+ * Escriba una función recursiva que calcule la suma de los primeros n números naturales ingresados
+ * por el usuario. Por ejemplo, si el usuario ingresa 5, el programa debe devolver 15 (1 + 2 + 3 + 4 + 5).
+ */
+
+public class Ejercicio1 {
+}
---
+++ src/main/java/co/edu/uniquindio/programacion4/seguimiento/Ejercicio2.java
@@ -0,0 +1,11 @@
+package co.edu.uniquindio.programacion4.seguimiento;
+
+/*
+* Implemente un programa usando backtracking para resolver el siguiente problema:
+* Dado un conjunto de números sin repetición, genere todas las permutaciones posibles de esos números.
+* Por ejemplo, para el conjunto [1, 2, 3], el programa debe generar:
+* [1, 2, 3] [1, 3, 2] [2, 1, 3] [2, 3, 1] [3, 1, 2] [3, 2, 1]
+ */
+
+public class Ejercicio2 {
+}
