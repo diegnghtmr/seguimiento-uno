850b212e7d158177ced10ef4f1efd887f8f2da92
Diego Flores <124117944+diegnghtmr@users.noreply.github.com>
2025-02-05 14:31:54 -0500

    Resolved

--- src/main/java/co/edu/uniquindio/programacion4/seguimiento/Ejercicio1.java
+++ src/main/java/co/edu/uniquindio/programacion4/seguimiento/Ejercicio1.java
@@ -1,4 +1,5 @@
 package co.edu.uniquindio.programacion4.seguimiento;
+import java.util.Scanner;
 
 /*
  * Escriba una función recursiva que calcule la suma de los primeros n números naturales ingresados
@@ -6,4 +7,19 @@ package co.edu.uniquindio.programacion4.seguimiento;
  */
 
 public class Ejercicio1 {
+    public static void main(String[] args) {
+        Scanner lectura = new Scanner (System.in);
+        System.out.print("Ingrese un número: ");
+        int n = lectura.nextInt();
+
+        System.out.println("La suma es: " + sumarNaturales(n));
+    }
+
+    public static int sumarNaturales(int n) {
+        if (n == 0) {
+            return 0;
+        } else {
+            return n + sumarNaturales(n - 1);
+        }
+    }
 }
--- src/main/java/co/edu/uniquindio/programacion4/seguimiento/Ejercicio2.java
+++ src/main/java/co/edu/uniquindio/programacion4/seguimiento/Ejercicio2.java
@@ -8,4 +8,43 @@ package co.edu.uniquindio.programacion4.seguimiento;
  */
 
 public class Ejercicio2 {
+
+    public static void main(String[] args) {
+        int[] conjunto = {1, 2, 3};
+        permutar(conjunto, 0);
+    }
+
+    // Función principal para generar permutaciones a partir de una posición dada (indice)
+    private static void permutar(int[] arreglo, int indice) {
+        if (indice == arreglo.length) {
+            imprimirArreglo(arreglo, 0);
+            System.out.println();
+        } else {
+            permutarRecursivo(arreglo, indice, indice);
+        }
+    }
+
+    // Función auxiliar que simula la iteración desde 'i' hasta el final del arreglo
+    private static void permutarRecursivo(int[] arreglo, int indice, int i) {
+        if (i < arreglo.length) {
+            intercambiar(arreglo, indice, i);
+            permutar(arreglo, indice + 1);
+            intercambiar(arreglo, indice, i);
+            permutarRecursivo(arreglo, indice, i + 1);
+        }
+    }
+
+    // Método auxiliar para intercambiar dos elementos en el arreglo
+    private static void intercambiar(int[] arreglo, int i, int j) {
+        int temp = arreglo[i];
+        arreglo[i] = arreglo[j];
+        arreglo[j] = temp;
+    }
+
+    private static void imprimirArreglo(int[] arreglo, int indice) {
+        if (indice < arreglo.length) {
+            System.out.print(arreglo[indice] + " ");
+            imprimirArreglo(arreglo, indice + 1);
+        }
+    }
 }
